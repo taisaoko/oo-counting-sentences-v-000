6551065005a08c91d0e2f800327b3387bfc4a2aa
taisaoko <ttd3calvin@gmail.com>
2018-08-31 13:25:18 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
-  end
+    self.end_with?("?") 
 
   def exclamation?
-
+    self.end_with?("!") 
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).count
   end
+  
 end
